6e05558cb5151d2212cd8eab195204badb9a4992
dnl <danieldavid2001@gmail.com>
2021-02-18 20:25:19 -0500

    Fun

--- Análisis numérico/Lab7/code/lsquare.m
+++ Análisis numérico/Lab7/code/lsquare.m
@@ -7,17 +7,18 @@ function output = lsquare(pointMatrix)
     [vS, hS] = size(pointMatrix);
 
     if hS == 2 && vS > 1
-        sumX = sum(pointMatrix(:,1));
-        sumY = sum(pointMatrix(:,2));
-        sumX2 = sum(pointMatrix(:,1).^2);
-        sumXY = sum(pointMatrix(:,1).*pointMatrix(:,2));
+        sumX = sum(pointMatrix(:,1))
+        sumY = sum(pointMatrix(:,2))
+        sumX2 = sum(pointMatrix(:,1).^2)
+        sumXY = sum(pointMatrix(:,1).*pointMatrix(:,2))
         N = vS;
 
         coTrix = [sumX2, sumX; sumX, N];
         anTrix = [sumXY; sumY];
 
-        vals = linsolve(coTrix, anTrix);
+        vals = linsolve(coTrix, anTrix)
 
         output = vals(1)*x+vals(2);
     end
 end
+
