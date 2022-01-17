70547f577eadf248305d22597dc762d4833e332d
MrDahaniel <danieldavid2001@gmail.com>
2022-01-16 23:36:09 -0500

    Memes

--- 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/pyFiles/park.py
+++ 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/pyFiles/park.py
@@ -2,3 +2,18 @@ class Park:
     def __init__(self) -> None:
         self.currentTime: int = 800
         self.attractions: dict = {}
+
+
+class Location:
+    def __init__(self, name: str, serviceRate: int, duration: int) -> None:
+        self.name: str = name
+        self.serviceRate: int = serviceRate
+        self.waitTime: int = 0
+        self.fakeWaitTime: int = 0
+        self.duration: int = 0
+        self.
+        
+
+class Hub(Location):
+    def __init__(self) -> None:
+        super().__init__()
--- 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/pyFiles/person.py
+++ 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/pyFiles/person.py
@@ -1,12 +1,16 @@
-from __future__ import annotations
+import imp
+from park import Location
 
+class Archetype:
+    def __init__(self) -> None:
+        pass
 
 class Person:
     def __init__(self, id, name: str = "Jon Arbuckle", archetype: dict = {}) -> None:
         self.id: str = id
         self.name: str = name
-        self.currentLocation: str = "Hub"
-        self.totalAttractions: int = 0
+        self.currentLocation: Location = 
+        self.attractionsExperienced: int = 0
         self.totalWaitTime: int = 0
         self.arrivalTime: int = None
         self.departureTime: int = None
@@ -19,4 +23,5 @@ class Person:
     def spinRoulette():
         pass
 
-    def  
+    def joinQueue():
+        pass
