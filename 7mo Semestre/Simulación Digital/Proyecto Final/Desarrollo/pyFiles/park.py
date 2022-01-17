8cde3a599c58d84b3869beaed3257e75b79a999c
MrDahaniel <danieldavid2001@gmail.com>
2022-01-17 08:40:56 -0500

    Fun

--- 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/pyFiles/park.py
+++ 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/pyFiles/park.py
@@ -1,7 +1,11 @@
+from person import Person
+
+
 class Park:
     def __init__(self) -> None:
         self.currentTime: int = 800
         self.attractions: dict = {}
+        self.activities: list()
 
 
 class Location:
@@ -11,8 +15,8 @@ class Location:
         self.waitTime: int = 0
         self.fakeWaitTime: int = 0
         self.duration: int = 0
-        self.
-        
+        self.queue: list(Person) = []
+
 
 class Hub(Location):
     def __init__(self) -> None:
--- 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/pyFiles/person.py
+++ 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/pyFiles/person.py
@@ -1,15 +1,16 @@
 import imp
 from park import Location
 
+
 class Archetype:
     def __init__(self) -> None:
         pass
+
 
 class Person:
     def __init__(self, id, name: str = "Jon Arbuckle", archetype: dict = {}) -> None:
         self.id: str = id
         self.name: str = name
-        self.currentLocation: Location = 
         self.attractionsExperienced: int = 0
         self.totalWaitTime: int = 0
         self.arrivalTime: int = None
