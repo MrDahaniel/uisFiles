1671dd97addcc22c7593194faa276de4414579a0
MrDahaniel <danieldavid2001@gmail.com>
2022-01-17 23:57:14 -0500

    Person update

--- 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/pyFiles/person.py
+++ 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/pyFiles/person.py
@@ -1,14 +1,15 @@
-import imp
+from random import uniform
+from numpy import random
 from park import Location
 
 
 class Archetype:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, attractionChance) -> None:
+        self.attractionChance: float = attractionChance
 
 
 class Person:
-    def __init__(self, id, name: str = "Jon Arbuckle", archetype: dict = {}) -> None:
+    def __init__(self, id: int, name: str, archetype: Archetype) -> None:
         self.id: str = id
         self.name: str = name
         self.attractionsExperienced: int = 0
@@ -18,8 +19,11 @@ class Person:
         self.archetype: dict = archetype
         self.activities: list(tuple) = []
 
-    def flipCoin():
-        pass
+    def flipCoin(self):
+        if random.uniform(low=0, high=1) < self.archetype.attractionChance:
+            pass
+        else:
+            pass
 
     def spinRoulette():
         pass
