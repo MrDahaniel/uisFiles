de7f7424d15b7e1f24fd4bf748ce8830361d3587
MrDahaniel <danieldavid2001@gmail.com>
2022-02-13 23:14:01 -0500

    Fun

--- 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/simuPark/park.py
+++ 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/simuPark/park.py
@@ -1,13 +1,29 @@
-from simuPark.person import Person
+from math import gamma
+from random import choice
+from typing import Callable
+from simuPark.person import Person, Archetype
+from simuPark.constants import ACTIVITIES, ARCHETYPES, ATTRACTIONS
+import numpy as np
 
 
 class Queue:
     def __init__(self, type: str = "NORMAL") -> None:
         self.type: str = type
+        self.waitTime: int = 0
         self.inQueue: list[Person] = []
 
-    def serve(self, nPeople: int):
-        pass
+    def serve(self, name: str, nPeople: int, duration: int):
+        # In this scenario, the number of people in queue is less or the same as the
+        # service rate of the attraction, everyone on the queue is served.
+        if nPeople > len(self.inQueue):
+            nPeople = len(self.inQueue)
+
+        for _i in range(nPeople):
+            person: Person = self.inQueue.pop(0)
+            person.rideAttraction(name=name, duration=duration)
+
+    def addToQueue(self, person: Person):
+        self.inQueue.append(person)
 
 
 class Activity:
@@ -28,11 +44,26 @@ class Attraction(Activity):
     ) -> None:
         super().__init__(name, popularity, duration)
         self.serviceRate: int = serviceRate
-        self.waitTime: int = 0
         self.fakeWaitTime: int = 0
         self.queue: Queue = Queue()
         if altQueue in ["DFP", "SFP"]:
             self.altQueue: Queue = Queue(type=altQueue)
+
+    def serve(self):
+        # We calculate the amount of people the attraction can serve each minute
+        nPeopleToServe = np.floor(self.serviceRate / 60)
+        # We tell out Queue to serve the amount of people given
+        # And the duration of the ride
+        self.queue.serve(name=self.name, nPeople=nPeopleToServe, duration=self.duration)
+
+    def addToQueue(self, person: Person, queue: str):
+        if queue == "NORMAL":
+            self.queue.addToQueue(person)
+        else:  # queue in ["DFP", "SFP"]
+            self.altQueue.addToQueue(person)
+
+    def serveWithFastPass(self):
+        pass
 
     def updateWaitTime(self):
         pass
@@ -44,17 +75,124 @@ class Attraction(Activity):
 class Park:
     def __init__(
         self,
-        attractions: list[Attraction],
-        activities: list[Activity],
+        attrDict: dict[str, dict] = ATTRACTIONS,
+        activitiesDict: dict[str, dict] = ACTIVITIES,
+        archetypeDict: dict[str, dict] = ARCHETYPES,
+        function: Callable = lambda x, k: np.power(k, x) * np.exp(-k) / gamma(x + 1),
+        altQ: str = None,
         hoursOpen: int = 11,
     ) -> None:
         self.currentTime: int = 0
         self.closingTime: int = 60 * hoursOpen
-        self.attractions: list[Attraction] = attractions
-        self.activities: list[Activity] = activities
+        self.attractions: list[Attraction] = self._handleAttractions(attrDict, altQ)
+        self.activities: list[Activity] = self._handleActivities(activitiesDict)
+        self.guestArchetypes: list[Archetype] = self._handleArchetypes(archetypeDict)
+        self.guests: list[Person] = []
+        self.function: Callable[[int], float] = function
 
-    def startDay(self, totalVisitors: int):
-        pass
+    def startDayBase(self, totalGuests: int) -> None:
+        # Day starts, time starts running minute pero minute depending in the
+        # hours open
+        for minute in range(self.closingTime):
+            self._receiveGuests(totalGuests=totalGuests, time=minute)
+            for guest in self.guests:
+                guest.checkLeavePark(minute)
+                # This case is the 'left the park' state, they're skipped
+                if guest.timeLeftInActivity == -2:
+                    continue
 
-    def receiveVisitors(self):
+                # In this case, the guest is in a queue, they're are skipped
+                # as they're not able to change selections. TotalWaitTime increases.
+                elif guest.timeLeftInActivity == -1:
+                    guest.totalWaitTime += 1
+                    continue
+
+                # On this case, the guest is currently doing an activity
+                # or riding an attraction. Time passes.
+                elif guest.timeLeftInActivity > 0:
+                    guest.timeLeftInActivity -= 1
+                    continue
+
+                # In this scenario, they're looking for something to do
+                # They're free to choose based on their archetype
+                elif guest.timeLeftInActivity == 0:
+                    selection = guest.chooseWhatToDo(self.activities, self.attractions)
+                    if isinstance(selection, Attraction):
+                        # guest.checkAttraction(selection)
+                        guest.timeLeftInActivity = -1
+                        pass
+                    elif isinstance(selection, Activity):
+                        guest.doActivity(selection.name, selection.duration)
+
+    def _handleArchetypes(self, archetypeDict: dict[str, dict]) -> list[Archetype]:
+        # setArchetypes handles the creation of the archetypes
+        # to be used during the simulation.
+
+        if not bool(archetypeDict):
+            raise IndexError("Dictionary is Empty") from None
+
+        archetypeList: list[Archetype] = []
+
+        for name, details in archetypeDict.items():
+            archetype: Archetype = Archetype(
+                name=name,
+                attractionChance=details.get("AttractionPercentage"),
+                maxStay=details.get("MaxStayTime"),
+                minStay=details.get("MinStayTime"),
+                maxWaitTime=details.get("MaxWaitTime"),
+                minWaitTime=details.get("MinWaitTime"),
+            )
+            archetypeList.append(archetype)
+
+        return archetypeList
+
+    def _handleActivities(self, activityDict: dict[str, dict]) -> list[Activity]:
+        if not bool(activityDict):
+            raise IndexError("Dictionary is Empty") from None
+
+        activityList: list[Archetype] = []
+
+        for name, details in activityDict.items():
+            activity: Activity = Activity(
+                name=name,
+                duration=details.get("Duration"),
+                popularity=details.get("Popularity"),
+            )
+            activityList.append(activity)
+
+        return activityList
+
+    def _handleAttractions(
+        self, attractionDict: dict[str, dict], atlQueue: str = None
+    ) -> list[Attraction]:
+        if not bool(attractionDict):
+            raise IndexError("Dictionary is Empty") from None
+
+        attractionList: list[Archetype] = []
+
+        for name, details in attractionDict.items():
+            attraction: Attraction = Attraction(
+                name=name,
+                duration=details.get("Duration"),
+                popularity=details.get("Popularity"),
+                serviceRate=details.get("ServiceRate"),
+                altQueue=atlQueue,
+            )
+            attractionList.append(attraction)
+
+        return attractionList
+
+    def _receiveGuests(self, totalGuests: int, time: int) -> None:
+        # This function is executed everytime a minute passes
+        # It handles the creation of each person that visits the park
+        randNum = np.random.uniform(low=0, high=1)
+        nGuests = int(np.floor(totalGuests / self.closingTime))
+
+        if randNum <= float(self.function(time / 60, 3)):
+            for _i in range(nGuests):
+                archetype: Archetype = choice(self.guestArchetypes)
+                newGuest: Person = Person(len(self.guests), time, archetype)
+                self.guests.append(newGuest)
+
+    def passTime(self):
         pass
--- 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/simuPark/person.py
+++ 7mo Semestre/Simulación Digital/Proyecto Final/Desarrollo/simuPark/person.py
@@ -1,6 +1,7 @@
+from __future__ import annotations
 from random import uniform, randint, choices
 from numpy import random
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
     from simuPark.park import Activity, Attraction
@@ -42,31 +43,83 @@ class Person:
         self.departureTime: int = (
             randint(archetype.minStay, archetype.maxStay) + arrivalTime
         )
-        self.bulkTime: int = randint(archetype.minWaitTime, archetype.maxWaitTime)
+        self.maxWait: int = randint(archetype.minWaitTime, archetype.maxWaitTime)
         self.attractionChance: float = archetype.attractionChance
 
     # General functions, used in all scenarios
-    def flipCoin(self):
-        if random.uniform(low=0, high=1) < self.archetype.attractionChance:
-            # Person chooses to do queue for an attraction
-            pass
-        else:
-            # Person chooses to do an activity
-            pass
 
-    def spinRoulette(self, activities: list[Activity]) -> Activity:
-        totalPopularity: int = sum([activity.popularity for activity in activities])
+    def report(self):
+        return f"{self.id}:  arvTime: {self.arrivalTime} thingsDone: {self.thingsDone} attrExp: {self.attractionsExperienced} "
+
+    # Person does an activity, it gets set to idle while doing so
+    def doActivity(self, name: str, duration: int):
+        self.currentActivity = name
+        self.timeLeftInActivity = duration
+
+        # !!!!!!!!!!!!! DELETE LATER !!!!!!!!!!!!!!!!!!
+        self.thingsDone.append(name)
+
+    # Person rides an attraction, it adds the activity done to the list
+    # and sets it to idle for the duration of the ride
+    def rideAttraction(self, name: str, duration: int):
+        self.thingsDone.append(name)
+        self.currentActivity = name
+        self.attractionsExperienced += 1
+        self.timeLeftInActivity = duration
+
+    def chooseWhatToDo(
+        self,
+        activities: list[Activity],
+        attractions: list[Attraction],
+    ) -> Union[Activity, Attraction]:
+        # If true, person decides to do an attraction; else it's an activity
+        if self.flipWeightedCoin():
+            return self.spinRoulette(attractions)
+        else:
+            return self.spinRoulette(activities)
+
+    def flipWeightedCoin(self) -> bool:
+        # If true, person decides to do an attraction; else it's an activity
+        return random.uniform(low=0, high=1) < self.attractionChance
+
+    def spinRoulette(
+        self,
+        options: list[Union[Activity, Attraction]],
+    ) -> Union[Activity, Attraction]:
+        totalPopularity: int = sum([activity.popularity for activity in options])
         weighedPopularity: list[float] = [
-            activity.popularity / totalPopularity for activity in activities
+            activity.popularity / totalPopularity for activity in options
         ]
-        return choices(population=activities, weights=totalPopularity, k=1)[0]
+        return choices(population=options, weights=weighedPopularity, k=1)[0]
+
+    def checkAttraction(self, attraction: Attraction):
+        # This is the vanilla case, we just need to check if the wait time
+        # is less than the max wait time.
+        # This also can be used if the person doesn't have a turboPass
+        # in the Salitre FP scenario
+        if attraction.queue.waitTime < self.maxWait:
+            self.timeLeftInActivity = -1
+            attraction.addToQueue(self, "NORMAL")
 
     def joinQueue(self, attraction: Attraction, queue: str = "NORMAL"):
-        pass
+        # timeLeftInActivity is set to -1 to indicate person being in a queue, this way
+        # it can be added to the totalWaitTime on the next cycle
+        self.timeLeftInActivity = -1
+        attraction.addToQueue(self, queue)
 
-    def leavePark(self):
-        pass
+    def checkLeavePark(self, time: int):
+        # This sets the timeLeftInActivity to -2
+        # this scenario implies the guest left the park, they are ignored during the
+        # check when time passes
+        if self.departureTime >= time and self.timeLeftInActivity != 0:
+            self.timeLeftInActivity = -2
 
     # Disney FastPass specific
-    def checkAttraction(self, attraction: Attraction):
-        pass
+    def checkAttractionFP(self, attraction: Attraction):
+        # First, we check the wait time for the attraction
+        # if the wait time is less than 30 minutes, person will join
+        if attraction.queue.waitTime <= 30:
+            # We set the timeLeftInActivity to -1 to identify as the person
+            # is waiting on queue
+            self.timeLeftInActivity = -1
+            attraction.addToQueue(self, "NORMAL")
