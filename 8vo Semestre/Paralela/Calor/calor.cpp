bc02400cce2855aeaa07c4e0e85eeb88c49ba08d
MrDahaniel <danieldavid2001@gmail.com>
2022-05-13 09:24:07 -0500

    Fun

--- 8vo Semestre/Paralela/Calor/calor.cpp
+++ 8vo Semestre/Paralela/Calor/calor.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <tuple>
 
+int SIZE, STEPS, TIME;
+int NEIGHBOURS = 2;
+
 int get_flag_value(int argc, char* argsv[], std::string flag) {
     bool arg = false;
     int n = -1;
@@ -22,20 +25,68 @@ int get_flag_value(int argc, char* argsv[], std::string flag) {
     return n;
 }
 
-double* generate_bar(int size, double (*fn)(int, int)) {
-    double* bar = (double*)malloc(size * sizeof(double));
+double* generate_bar(double (*fn)(int, double)) {
+    double* bar = (double*)malloc(SIZE * sizeof(double));
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < SIZE; i++) {
         bar[i] = fn(i, 0);
     }
 
     return bar;
 }
 
-double T(int length, int time) {
+double* calculate_next_bar(double* bar) {
+    double* new_bar = (double*)malloc(SIZE * sizeof(double));
+
+    for (int i = 0; i < SIZE; i++) {
+        new_bar[i] = (bar[i - 1] + bar[i + 1]) / NEIGHBOURS;
+    }
+
+    free(bar);
+
+    return new_bar;
 }
 
-void calculate_step(double* bar) {
+double* simulate_bar(double* bar, double (*fn)(int, double)) {
+    double* simu_bar = bar;
+
+    const double delta = TIME / STEPS;
+    double cur_time = 0;
+
+    for (int i = 0; i < STEPS; i++) {
+        cur_time += delta;
+
+        simu_bar = calculate_next_bar(simu_bar);
+
+        for (int j = 0; j < SIZE; j++) {
+            double fn_defd = fn(j, cur_time);
+            if (fn_defd > 0) {
+                simu_bar[j] = fn_defd;
+            }
+        }
+    }
+
+    return simu_bar;
+}
+
+double T(int length, double _time) {
+    const int MAX_TEMP = 100;
+    const int MIN_TEMP = 0;
+
+    if (length == 0 || length == SIZE - 1) {
+        // returns the max temp
+        return MAX_TEMP;
+    } else {
+        // returns the min temp, in this case 0
+        return MIN_TEMP;
+    }
+}
+
+void print_bar(double* bar) {
+    for (int i = 0; i < SIZE; i++) {
+        printf("%f ", bar[i]);
+    }
+    printf("\n");
 }
 
 int main(int argc, char* argsv[]) {
@@ -43,10 +94,17 @@ int main(int argc, char* argsv[]) {
     const std::string time_flag = "-t";
     const std::string steps_flag = "-s";
 
-    int size = get_flag_value(argc, argsv, size_flag);
-    int delta = get_flag_value(argc, argsv, time_flag) / get_flag_value(argc, argsv, steps_flag);
+    SIZE = get_flag_value(argc, argsv, size_flag);
+    TIME = get_flag_value(argc, argsv, time_flag);
+    STEPS = get_flag_value(argc, argsv, steps_flag);
 
-    double* bar = generate_bar(size, &T);
+    double* bar = generate_bar(&T);
+
+    printf("%s\n    ", "Initial Bar:");
+    print_bar(bar);
+
+    printf("%s\n    ", "Final Bar");
+    print_bar(simulate_bar(bar, &T));
 
     return 0;
 }
