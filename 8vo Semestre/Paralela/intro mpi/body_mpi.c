11475af7f00ca5b977228be22e7402813b35e751
dnl <danieldavid2001@gmail.com>
2022-07-18 23:44:08 -0500

    Dank

--- 8vo Semestre/Paralela/intro mpi/body_mpi.c
+++ 8vo Semestre/Paralela/intro mpi/body_mpi.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MIN_RANKS 3
+
 double const G = 6.67e-11;
 int const DELTA_T = 86400;
 int const N_STEPS = (365 * 10) - 1;
@@ -58,56 +60,43 @@ struct vector3d calculate_unitary_vector(struct vector3d vec_A, struct vector3d 
     };
 }
 
-void calculate_accelerations(struct space_body* bodies) {
-    for (int i = 0; i < TOTAL_BODIES; i++) {
-        bodies[i].acceleration.x = 0;
-        bodies[i].acceleration.y = 0;
-        bodies[i].acceleration.z = 0;
+struct space_body calculate_body_prop(struct space_body* bodies, int body_id) {
+    struct vector3d new_acceleration = (struct vector3d){0, 0, 0};
 
-        for (int j = 0; j < TOTAL_BODIES; j++) {
-            if (i == j) continue;
+    for (int j = 0; j < TOTAL_BODIES; j++) {
+        if (body_id == j) continue;
 
-            double F = (bodies[j].mass /
-                        pow(calculate_magnitude(
-                                bodies[i].position,
-                                bodies[j].position),
-                            2));
+        double F = (bodies[j].mass /
+                    pow(calculate_magnitude(
+                            bodies[body_id].position,
+                            bodies[j].position),
+                        2));
+        struct vector3d unit_vector = calculate_unitary_vector(
+            bodies[body_id].position,
+            bodies[j].position);
 
-            struct vector3d unit_vector = calculate_unitary_vector(
-                bodies[i].position,
-                bodies[j].position);
-
-            bodies[i].acceleration.x += -G * F * unit_vector.x;
-            bodies[i].acceleration.y += -G * F * unit_vector.y;
-            bodies[i].acceleration.z += -G * F * unit_vector.z;
-        }
+        new_acceleration.x += -G * F * unit_vector.x;
+        new_acceleration.y += -G * F * unit_vector.y;
+        new_acceleration.z += -G * F * unit_vector.z;
     }
 
-    return;
-}
+    struct vector3d new_speed = (struct vector3d){
+        new_acceleration.x * DELTA_T + bodies[body_id].speed.x,
+        new_acceleration.y * DELTA_T + bodies[body_id].speed.y,
+        new_acceleration.z * DELTA_T + bodies[body_id].speed.z};
 
-void update_speeds(struct space_body* bodies) {
-    for (int i = 0; i < TOTAL_BODIES; i++) {
-        bodies[i].speed.x = bodies[i].acceleration.x * DELTA_T + bodies[i].speed.x;
-        bodies[i].speed.y = bodies[i].acceleration.y * DELTA_T + bodies[i].speed.y;
-        bodies[i].speed.z = bodies[i].acceleration.z * DELTA_T + bodies[i].speed.z;
-    }
-}
+    struct vector3d new_position = (struct vector3d){
+        new_speed.x * DELTA_T + bodies[body_id].position.x,
+        new_speed.y * DELTA_T + bodies[body_id].position.y,
+        new_speed.z * DELTA_T + bodies[body_id].position.z};
 
-void update_positions(struct space_body* bodies) {
-    for (int i = 0; i < TOTAL_BODIES; i++) {
-        bodies[i].position.x = bodies[i].speed.x * DELTA_T + bodies[i].position.x;
-        bodies[i].position.y = bodies[i].speed.y * DELTA_T + bodies[i].position.y;
-        bodies[i].position.z = bodies[i].speed.z * DELTA_T + bodies[i].position.z;
-    }
-}
-
-void pass_time(struct space_body* bodies, int n_delta_ts) {
-    for (int i = 0; i < n_delta_ts; i++) {
-        calculate_accelerations(bodies);
-        update_speeds(bodies);
-        update_positions(bodies);
-    }
+    return (struct space_body){
+        bodies[body_id].name,
+        new_position,
+        new_speed,
+        new_acceleration,
+        bodies[body_id].mass,
+    };
 }
 
 void print_bodies(struct space_body* bodies) {
@@ -135,56 +124,90 @@ void print_from_center(struct space_body* bodies) {
 }
 
 int main(int argc, char** argsv) {
-    struct space_body* bodies = (struct space_body*)malloc(sizeof(struct space_body) * TOTAL_BODIES);
+    int rank, size;
+    int body_id;
 
-    bodies[0] = (struct space_body){
-        "sun",                       // Name
-        (struct vector3d){0, 0, 0},  // Position
-        (struct vector3d){0, 0, 0},  // Speed
-        (struct vector3d){0, 0, 0},  // Acceleration
-        1.989e30,                    // Mass
-    };
+    MPI_Status status;
 
-    bodies[1] = (struct space_body){
-        "mercury",                          // Name
-        (struct vector3d){57.909e9, 0, 0},  // Position
-        (struct vector3d){0, 47.36e3, 0},   // Speed
-        (struct vector3d){0, 0, 0},         // Acceleration
-        0.33011e24,                         // Mass
-    };
+    // Inicializar MPI
+    MPI_Init(&argc, &argsv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    bodies[2] = (struct space_body){
-        "venus",                             // Name
-        (struct vector3d){108.209e9, 0, 0},  // Position
-        (struct vector3d){0, 35.02e3, 0},    // Speed
-        (struct vector3d){0, 0, 0},          // Acceleration
-        4.8675e24                            // Mass
-    };
+    if (size < MIN_RANKS) {
+        fprintf(stderr, "Error: Minimun number of ranks is not met.\n%d needed, %d available.\n", MIN_RANKS, size);
+        exit(-1);
+    }
 
-    bodies[3] = (struct space_body){
-        "earth",                             // Name
-        (struct vector3d){149.596e9, 0, 0},  // Position
-        (struct vector3d){0, 29.78e3, 0},    // Speed
-        (struct vector3d){0, 0, 0},          // Acceleration
-        5.9724e24                            // Mass
-    };
+    // Usamos el rank 0 para hacer de coordinador de todo lo que está pasando
+    if (rank == 0) {
+        struct space_body* bodies = (struct space_body*)malloc(sizeof(struct space_body) * TOTAL_BODIES);
 
-    bodies[4] = (struct space_body){
-        "mars",                              // Name
-        (struct vector3d){227.923e9, 0, 0},  // Position
-        (struct vector3d){0, 24.07e3, 0},    // Speed
-        (struct vector3d){0, 0, 0},          // Acceleration
-        0.64171e24                           // Mass
-    };
+        bodies[0] = (struct space_body){
+            "sun",                       // Name
+            (struct vector3d){0, 0, 0},  // Position
+            (struct vector3d){0, 0, 0},  // Speed
+            (struct vector3d){0, 0, 0},  // Acceleration
+            1.989e30,                    // Mass
+        };
 
-    printf("\nCondiciones Iniciales:\n");
+        bodies[1] = (struct space_body){
+            "mercury",                          // Name
+            (struct vector3d){57.909e9, 0, 0},  // Position
+            (struct vector3d){0, 47.36e3, 0},   // Speed
+            (struct vector3d){0, 0, 0},         // Acceleration
+            0.33011e24,                         // Mass
+        };
 
-    print_bodies(bodies);
+        bodies[2] = (struct space_body){
+            "venus",                             // Name
+            (struct vector3d){108.209e9, 0, 0},  // Position
+            (struct vector3d){0, 35.02e3, 0},    // Speed
+            (struct vector3d){0, 0, 0},          // Acceleration
+            4.8675e24                            // Mass
+        };
 
-    pass_time(bodies, N_STEPS);
+        bodies[3] = (struct space_body){
+            "earth",                             // Name
+            (struct vector3d){149.596e9, 0, 0},  // Position
+            (struct vector3d){0, 29.78e3, 0},    // Speed
+            (struct vector3d){0, 0, 0},          // Acceleration
+            5.9724e24                            // Mass
+        };
 
-    printf("\nCondiciones Finales:\n");
-    print_bodies(bodies);
+        bodies[4] = (struct space_body){
+            "mars",                              // Name
+            (struct vector3d){227.923e9, 0, 0},  // Position
+            (struct vector3d){0, 24.07e3, 0},    // Speed
+            (struct vector3d){0, 0, 0},          // Acceleration
+            0.64171e24                           // Mass
+        };
 
+        printf("\nCondiciones Iniciales:\n");
+
+        print_bodies(bodies);
+
+        int current_rank = 1;
+
+        for (int time = 0; time < N_STEPS; time++) {
+            MPI_Send(&body_id, 1, MPI_INT, current_rank, 10, MPI_COMM_WORLD);
+        }
+
+        printf("\nCondiciones Finales:\n");
+        print_bodies(bodies);
+    } else {
+        do {
+            MPI_Recv(&body_id, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+            if (body_id == -1) {
+                printf("Rank #%d is out.\n", rank);
+                break;
+            }
+
+        } while (1);
+    }
+
+    // Chado MPI
+    MPI_Finalize();
     return 0;
 }
